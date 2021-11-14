08e9a943252479776aa53a6d8f5f68b5e14ae707
duckytutorials <92290826+duckytutorials@users.noreply.github.com>
2021-11-14 13:25:07 +0630

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,197 @@
+module.exports = ({
+  userav: "0",
+  //music 
+   file: "server.js", //For reboot and stats//
+   database: "./database/main/main_scheme_1.sql", //For stats//
+ 
+   pause: "⏸️ Paused.",
+   resume: "▶️ Resumed!",
+   skip: "⏩ Skipped!",  //Available {song}//
+   stop: "⏹ Stopped.",  
+   remove: "Removed song on {d-amount}.", //Available {d-amount}//
+ 
+   clearsong: "✅ Cleared queue. from **{amount} song** to **0**", //Available {amount}//
+   shuffle: "Shuffle Queue.",
+   errorjoin: "{title:❌ You're not in a voice channel.} {color:FFFF00}",
+   errorqueue: "{title:❌ There no song was playing.} {color:FF0000}",
+ 
+   join: "Joined Voice Channel to the {join}.", //Available {join}//
+   dc: "Disconnected.",
+ 
+   //Changing Other//
+   clientidsoundcloud: "",
+   color: "000000",
+   permission: "2176183360",
+   userid: "default",
+   logmusic: "0",
+   247: "0", //0 = off | 1 = on stay 2 minutes | 2 = stay 24/7//
+   channelstatus: "895990877888053268", //Change to your channel id, to send message when the bot restart.// 
+   vol: "50", //Default Volume//
+   maxvol: "150", //Max Volume//
+   last: "null",
+   linkdownload: "",
+   filters: "none",
+   controlreact: "0",
+   saveseek: "0",
+   durationcache: "0",
+   reactmessageid: "",
+   nontrigger: "0", //for disable play message when react active//
+ 
+   customemoji1: "https://cdn.discordapp.com/emojis/852434440668184615.png?size=4096",
+   ytemoji: "https://cdn.discordapp.com/emojis/852432148207108110.png?size=4096",
+   scemoji: "https://cdn.discordapp.com/emojis/852432173758676993.png?size=4096",
+  
+   userused: "0",
+   commanduserused: "0",
+ 
+   //For playlist//
+   1: "",
+   2: "",
+   3: "",
+   4: "",
+   5: "",
+   6: "",
+   7: "",
+   8: "",
+   9: "",
+   10: "",
+     no: "❌",
+     hint: "2",
+ guess: "0",
+ msg: "0",
+     yes: "✅",
+     join: "",
+     up: ">",
+     prefix: ">",
+     wtitle: "",
+   wmsg: "",
+   wimg: "",
+   wchannel: "",
+   wdm: "disabled",
+   autorole: "",
+     imageauthor: 0,
+ imageresult: "",
+ imagelength: 0,
+ imagenumber: 0,
+ imagepage: 1,
+ bleft: "left|890706435955101747|false",
+ bright: "right|890706436278079499|false",
+ bno: "guilded_image_3833dadf524f7034bf|890299986695307374|false",
+ warn: "0",
+ mute: "",
+ levelling:"true",
+   level_msg:"{user.mention}, you just levelled up to {level}!",
+   level_channel:"",
+   level_roles:"",
+   level_order:"",
+   exp:"0",
+   level_card:"https://images-ext-2.discordapp.net/external/0Xv4kCUKHD1-XuITg1pfJKpexx0WNSDycImJsgMyMZg/%3Fwidth%3D473%26height%3D473/https/media.discordapp.net/attachments/901046126671691816/901047335671459881/Photo_1633692466619.jpg",
+   level_msges:"",
+   level_morder:"",
+   req:"100",
+   rank:"0",
+   snipe_msg: "",
+  snipe_author: "",
+  snipe_channel: "",
+  snipe_date: "",
+ msgEditorID: "undefined",
+ esnipeOldMsg: "undefined",
+ antilink: "false",
+  giveawaychannelid: 0,
+     giveawayguildid: 0,
+     giveawayprize: "",
+     giveawaydescription: "",
+     giveawaytime: 0,
+     giveawayparticipants: "",
+     giveawayisfinished: "false",
+     giveawayisgiveaway: "false",
+     testid1: "",
+     premium: "false",
+     money:"0",//Wallet
+   bank:"0",//Bank
+   work_msges:"",//Possible work messages
+   work_amounts:"",//Possible work amounts
+   work_cd:"10m",//Cooldown for work
+   symb:"<:economy:898404177955401759>",//Money symbol
+   bs:"<:economy:898404177955401759>",//Bank symbol
+   sb:"0",//Starting Bal
+   sbd:"false",//Starting Claimed or no
+   rs:"false",//Random spawns
+   ra:"",//Spawn amounts
+   rc:"",//Spawn blacklisted channels	
+   e:"false",//Economy Enabled Or no
+   bc:"1000000",//Maximum bank capacity
+   cf:"0",//Cock fight chances
+   c:"0",//Chicken count
+   cc:"0",//Chicken Cost
+   gc:"0",//Max gamble cost for RPS, Roulette and CF
+   ru:"",//Blacklisted economy users
+    afk_roles:"",//AFK needed roles
+   r:"",//Reason for AFK
+   time:"",//Time for AFK
+   afk_pings:"0",//Pings count in AFK
+   afk_mentions:"",//Mentions by which user in AFK
+   //Bot Logs
+   bl:"",//Bot Logs channel
+    afk: "",
+   EnglishOnly: "off",
+   welcomeBg: "",
+   welcomeChannel: "",
+ welcomeMsg: "",
+ verify: "on",
+ verifchannel: "900721610313584650",
+ verifrole: "900429109933535292",
+ cbw: "",
+   ticketchannel: "",
+     endstamp:"0",
+ hoster:"",
+ prize:"",
+ joined:"0",
+ joinedusers:"",
+ ended:"false",
+       item:"heh",//Item names
+      roles: "heh",
+       names:"heh",//Role shop messages
+       tmid:"a",
+       tmc:"",
+       tmm:"",
+       tm:"",
+       tc:"",
+       to:"",
+       t:"",
+     badwords: "0",
+     beg: "",
+         XP: "0",
+     Bank: "0",
+     Wallet: "0",
+     psuffix: '0',
+     tv: "0",
+     duffle: '0',
+     bag: "0",
+     smartphone: "0",
+     laptop: "0",
+     car: "0",
+     truck: "0",
+     helicopter: "0",
+     apartment: "0",
+     house: "0",
+     mansion: "0",
+     DailyChest: "0",
+     lucky: "0",
+     spiteful: "0",
+     PoorBadge: "0",
+         RichBadge: "0",
+         MillionerBadge: "0",
+     BillionaireBadge: "0",
+     shovel: "0",
+     fishingpole: "0",
+     fish: "0",
+     legendery: "0",
+     exotic: "0",
+     rare: "0",
+     duckey: "0",
+     spinner: "0",
+     pp: "0",
+     gun: "0",
+     dirt: "0",
+ });
